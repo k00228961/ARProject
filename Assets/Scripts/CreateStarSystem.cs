fc7309cbd7ae212acb4642cc16c018f462c300b1
k00228961 <k00228961@users.noreply.github.com>
2020-02-07 15:19:19 +0000

    Rep Update

---
+++ Assets/Scripts/CreateStarSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStarSystem : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    public int maxParticles = 100;
+    public TextAsset starCSV;
+    void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.maxParticles = maxParticles;
+        ParticleSystem.Burst[] bursts = new ParticleSystem.Burst[1];
+        bursts[0].minCount = (short)maxParticles;
+        bursts[0].maxCount = (short)maxParticles;
+        bursts[0].time = 0.0f;
+        particleSystem.emission.SetBursts(bursts, 1);
+    }
+
+    void Start()
+    {
+        string[] lines = starCSV.text.Split('\n');
+        ParticleSystem.Particle[] particleStars = new ParticleSystem.Particle[maxParticles];
+        particleSystem.GetParticles(particleStars);
+        for (int i = 0; i < maxParticles; i++)
+        {
+            string[] components = lines[i].Split(',');
+            particleStars[i].position = new Vector3(float.Parse(components[1]),
+                                                    float.Parse(components[3]),
+                                                    float.Parse(components[2]));
+            particleStars[i].remainingLifetime = Mathf.Infinity;
+        }
+        particleSystem.SetParticles(particleStars, maxParticles);
+    }
+}
